b2bf51f7c2b1e69f049b8157468d9eca45332b8b
poornima-alugubelly <reddypoornima683@gmail.com>
2021-08-27 13:15:07 +0530

    refactor : added image using innerHTML instead of div

--- index.js
+++ index.js
@@ -4,14 +4,13 @@ var check = document.querySelector("#check");
 var output = document.querySelector("#output");
 var close = document.querySelector(".close");
 var privacyNotice = document.querySelector(".privacy-notice");
-var image = document.querySelector(".image");
+
 function hideOnClick() {
 	privacyNotice.style.display = "none";
 }
 function calc() {
-	image.src = "";
 	if (Number(num.value) < 0) {
-		output.innerHTML = "Please enter a positive number";
+		output.innerHTML = `<div class="output-txt"> Please enter a positive number </div>`;
 		return 0;
 	}
 
@@ -27,14 +26,14 @@ function calc() {
 			date = Math.floor(date / 10);
 		}
 		if (sum % num.value === 0) {
-			output.innerHTML = "Congrats!! Yours is a lucky birthday";
-			image.src = "./img/leonardo.gif";
+			output.innerHTML = `<div class="output-txt"> Congrats!! Yours is a lucky birthday </div>;
+			                      <img class="image" src="/img/leonardo.gif">`;
 		} else {
-			output.innerHTML = "Sorry!! Yours is not a lucky birthday";
-			image.src = "./img/pika.gif";
+			output.innerHTML = `<div class="output-txt"> Sorry!! Yours is not a lucky birthday </div>;
+			                      <img class="image" src="/img/pika.gif">`;
 		}
 	} else {
-		output.innerHTML = "Please enter valid values for both fields";
+		output.innerHTML = `<div class="output-txt"> Please enter valid values for both fields </div>`;
 	}
 }
 
