abb9bc1e73f12be1e659a13bc22a2bbc24b64d64
poornima-reddy <70846516+poornima-alugubelly@users.noreply.github.com>
2022-01-14 10:54:43 +0530

    refactor: updated to Es6+

--- index.js
+++ index.js
@@ -1,41 +1,40 @@
-var calendar = document.querySelector("#calendar");
-var num = document.querySelector("#num");
-var check = document.querySelector("#check");
-var output = document.querySelector("#output");
-var close = document.querySelector(".close");
-var privacyNotice = document.querySelector(".privacy-notice");
+const calendar = document.querySelector("#calendar");
+const num = document.querySelector("#num");
+const check = document.querySelector("#check");
+const output = document.querySelector("#output");
+const close = document.querySelector(".close");
+const privacyNotice = document.querySelector(".privacy-notice");
 
-function hideOnClick() {
-	privacyNotice.style.display = "none";
-}
-function calc() {
-	if (Number(num.value) < 0) {
-		output.innerHTML = `<div class="output-txt"> Please enter a positive number </div>`;
-		return 0;
-	}
+const hideOnClick = () => (privacyNotice.style.display = "none");
 
-	if (num.value && calendar.value) {
-		var date = calendar.value;
-		date = Number(date.replaceAll("-", ""));
-		var sum = 0;
-		var last_digit = 0;
-		while (date > 0) {
-			last_digit = date % 10;
+function calcLuckyBirthday() {
+  if (Number(num.value) < 0) {
+    output.innerHTML = `<div class="output-txt"> Please enter a positive number </div>`;
+    return 0;
+  }
 
-			sum += last_digit;
-			date = Math.floor(date / 10);
-		}
-		if (sum % num.value === 0) {
-			output.innerHTML = `<div class="output-txt"> Congrats!! Yours is a lucky birthday </div>;
+  if (num.value && calendar.value) {
+    let date = calendar.value;
+    date = Number(date.replaceAll("-", ""));
+    let sum = 0;
+    let last_digit = 0;
+    while (date > 0) {
+      last_digit = date % 10;
+
+      sum += last_digit;
+      date = Math.floor(date / 10);
+    }
+    if (sum % num.value === 0) {
+      output.innerHTML = `<div class="output-txt"> Congrats!! Yours is a lucky birthday </div>;
 			                      <img class="image" src="/img/leonardo.gif">`;
-		} else {
-			output.innerHTML = `<div class="output-txt"> Sorry!! Yours is not a lucky birthday </div>;
+    } else {
+      output.innerHTML = `<div class="output-txt"> Sorry!! Yours is not a lucky birthday </div>;
 			                      <img class="image" src="/img/pika.gif">`;
-		}
-	} else {
-		output.innerHTML = `<div class="output-txt"> Please enter valid values for both fields </div>`;
-	}
+    }
+  } else {
+    output.innerHTML = `<div class="output-txt"> Please enter valid values for both fields </div>`;
+  }
 }
 
-check.addEventListener("click", calc);
+check.addEventListener("click", calcLuckyBirthday);
 close.addEventListener("click", hideOnClick);
